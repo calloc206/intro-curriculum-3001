39462cdbbd9f845e6c787765a75188cb9eb3e779
calloc206 <calloc206@gmail.com>
2020-08-11 03:30:02 +0000

    階乗関数の実装

--- app.js
+++ app.js
@@ -8,6 +8,10 @@
 function factorial(n) {
     let result = 1;
     // TODO このコメントを消して正しく実装してください。
+    let i=0;
+    for(i=1; i<=n; i++){
+        result *= i;        
+    }
     return result;
 }
 const assert = require('assert');
